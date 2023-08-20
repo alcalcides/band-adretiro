fbe981e7a35b5e84ca250c6a357d10f5e659d03f
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2023-08-20 02:51:40 -0300

    feat(worksheet): access data in a cell

--- src/model/Workbook.ts
+++ src/model/Workbook.ts
@@ -1,4 +1,4 @@
-import ExcelJS from "exceljs";
+import ExcelJS, { Worksheet } from "exceljs";
 
 export class Workbook {
     private workbook: ExcelJS.Workbook;
@@ -6,6 +6,22 @@ export class Workbook {
 
     constructor() {
         this.workbook = new ExcelJS.Workbook();
+    }
+
+    private async loadWorkbook(database: any): Promise<ExcelJS.Workbook> {
+        return await this.workbook.xlsx.load(database);
+    }
+
+    public async getSheet(dataBase: any, sheetName: string): Promise<ExcelJS.Worksheet>{
+        await this.loadWorkbook(dataBase)
+        const worksheet: Worksheet = this.workbook.getWorksheet(sheetName);
+        return worksheet;
+    }
+
+    public async getCellValue(dataBase: any, sheetName: string, cell: string) {
+        const sheet = await this.getSheet(dataBase, sheetName);
+        const data = sheet.getCell(cell);
+        return data.value
     }
     
     public async printSheets(database: any) {
